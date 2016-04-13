17b53a33485ec08d7c0b1dad3e3d5438a8311d30
derek <derek@derekdeMacBook-Pro.local>
2016-04-13 22:30:22 +0800

    A little bit refactor.

--- index.js
+++ index.js
@@ -3,53 +3,48 @@ function isDate(val) {
 }
 
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        root.deepmerge = factory();
-    }
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.deepmerge = factory();
+  }
 }(this, function () {
 
   return function deepmerge(target, src) {
-    var array = Array.isArray(src);
-    var dst = array && [] || {};
-
-    if (array) {
-      target = target || [];
-      dst = dst.concat(target);
-      src.forEach(function(e, i) {
-        if (typeof dst[i] === 'undefined' || isDate(dst[i])) {
-          dst[i] = e;
-        } else if (typeof e === 'object') {
-          dst[i] = deepmerge(target[i], e);
+    if (Array.isArray(src)) {
+      return src.reduce(function(sofar, curr, i) {
+        if (typeof sofar[i] === 'undefined' || isDate(sofar[i])) {
+          sofar[i] = curr;
+        } else if (typeof curr === 'object') {
+          sofar[i] = deepmerge(target[i], curr);
         } else {
-          if (target.indexOf(e) === -1) {
-            dst.push(e);
+          if (target.indexOf(curr) === -1) {
+            sofar.push(curr);
           }
         }
-      });
+        return sofar;
+      }, [].concat(target || []));
     } else {
+      var dest = {};
       if (target && typeof target === 'object') {
         Object.keys(target).forEach(function (key) {
-          dst[key] = target[key];
-        })
+          dest[key] = target[key];
+        });
       }
-      Object.keys(src).forEach(function (key) {
+      return Object.keys(src).reduce(function(sofar, key) {
         if (typeof src[key] !== 'object' || !src[key] || isDate(src[key])) {
-          dst[key] = src[key];
+          sofar[key] = src[key];
         } else {
           if (!target[key]) {
-            dst[key] = src[key];
+            sofar[key] = src[key];
           } else {
-            dst[key] = deepmerge(target[key], src[key]);
+            sofar[key] = deepmerge(target[key], src[key]);
           }
         }
-      });
+        return sofar;
+      }, dest);
     }
-
-    return dst;
   }
-
 }));
